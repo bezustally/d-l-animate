107ad3be1d6d23504bb849cbdbc7d880e62022e0
John Bezustally <ample-flood.0p@icloud.com>
2022-03-16 12:15:57 +0400

    style: prettier

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
+  const faq = document.querySelector('.faq');
 
-	let faq = document.querySelector('.faq')
+  faq.addEventListener('click', function (e) {
+    if (e.target.classList.contains('ask')) {
+      toogleItem(e.target);
+    }
+  });
 
-	faq.addEventListener('click', function(e) {
-		if (e.target.classList.contains('ask')) {
-			toogleItem(e.target)
-		}
-	})
-	function toogleItem(ask) {
-		let answer = ask.parentNode.querySelector('.answer')
-		let heightBefore = answer.offsetHeight
-		answer.classList.toggle('open')
-		let heightAfter = answer.offsetHeight
+  function toogleItem(ask) {
+    const answer = ask.parentNode.querySelector('.answer');
+    const heightBefore = answer.offsetHeight;
+    answer.classList.toggle('open');
+    const heightAfter = answer.offsetHeight;
 
-		if (answer.classList.contains('open')) {
-			answer.animate([
-				{ opacity: 0, transform: 'translateX(-100px)', height: 0 },
-				{ opacity: 1, transform: 'translateX(0)', height: heightAfter }
-			], { duration: 500 })
-		}
-		else {
-			answer.animate([
-				{ opacity: 1, transform: 'translateX(0)', height: heightBefore },
-				{ opacity: 0, transform: 'translateX(100px)', height: 0 },
-			], { duration: 500 })
-		}
-	}
-})
+    if (answer.classList.contains('open')) {
+      answer.animate(
+        [
+          { opacity: 0, transform: 'translateX(-100px)', height: 0 },
+          { opacity: 1, transform: 'translateX(0)', height: heightAfter },
+        ],
+        { duration: 500 }
+      );
+    } else {
+      answer.animate(
+        [
+          { opacity: 1, transform: 'translateX(0)', height: heightBefore },
+          { opacity: 0, transform: 'translateX(100px)', height: 0 },
+        ],
+        { duration: 500 }
+      );
+    }
+  }
+});
